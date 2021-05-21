b153630df275bbe7373823e91ffe4b3c3e5d40fe
Ahmad Alatab <ahmadatab86@gmail.com>
2021-05-21 00:42:30 -0700

    modifying components to use redux

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -3,13 +3,23 @@ import Home from './HomeComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import { View, Platform, StyleSheet, Text, ScrollView, Image } from 'react-native';
-import { createStackNavigator } from 'react-navigation-stack';
+import { Header, createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator, DrawerItems } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
 import SafeAreaView from 'react-native-safe-area-view';
+import { connect } from 'react-redux';
+import { fetchCampsites, fetchComments, fetchPromotions, fetchPartners } from '../redux/ActionCreators';
+
+const mapDispatchToProps = {
+    fetchCampsites,
+    fetchComments,
+    fetchPromotions,
+    fetchPartners
+};
+
 
 const DirectoryNavigator = createStackNavigator(
     {
@@ -222,6 +232,14 @@ const styles = StyleSheet.create({
 const AppNavigator = createAppContainer(MainNavigator)
 
 class Main extends Component {
+
+    componentDidMount() {
+        this.props.fetchCampsites();
+        this.props.fetchComments();
+        this.props.fetchPromotions();
+        this.props.fetchPartners();
+    }
+
     render() {
         return (
             <View style={{
@@ -234,4 +252,4 @@ class Main extends Component {
     }
 }
 
-export default Main;
+export default connect(null, mapDispatchToProps)(Main);
